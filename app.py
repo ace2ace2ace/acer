abd351a2acb3f438472873d9def006d1fa90af2a
ace2ace2ace <samkeu312@gmail.com>
2025-05-17 11:52:29 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import cohere
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+co = cohere.Client(os.getenv('COHERE_API_KEY'))
+
+
+@app.route('/')
+def home():
+
+    return app.send_static_file('index.html')
+
+
+@app.route('/generate-story', methods=['POST'])
+def generate_story():
+    data = request.json
+    format = data.get('format') 
+    topic = data.get('topic')
+    length = data.get('length')
+
+    prompt = f"""Write a {length} story with the following elements:
+    Format: {format}
+    Topic: {topic}
+
+    
+    Make it engaging and creative while maintaining a coherent narrative structure."""
+
+    response = co.generate(
+        model='command',
+        prompt=prompt,
+        max_tokens=2000,
+        temperature=0.8,
+        k=0,
+        stop_sequences=[],
+        return_likelihoods='NONE'
+    )
+
+    return jsonify({'story': response.generations[0].text})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
